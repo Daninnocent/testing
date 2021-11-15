370043846623af3aefc01c343542a9d6943c2098
Daninnocent <58911390+Daninnocent@users.noreply.github.com>
2021-11-15 09:54:12 +0700

    Update VideoState.hx

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -43,10 +43,6 @@ class VideoState extends MusicBeatState
 		
 		leSource = source;
 		transClass = toTrans;
-		if (frameSkipLimit != -1 && GlobalVideo.isWebm)
-		{
-			GlobalVideo.getWebm().webm.SKIP_STEP_LIMIT = frameSkipLimit;	
-		}
 	}
 	
 	override function create()
